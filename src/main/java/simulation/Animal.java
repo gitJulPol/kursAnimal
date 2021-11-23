9d4b241508b73fa42ccfb977da097ce054d39b52
Julia <jul.polak@hotmail.com>
2021-11-23 19:25:45 +0100

    lab 08, reproduce, new enum

--- src/main/java/simulation/Animal.java
+++ src/main/java/simulation/Animal.java
@@ -1,15 +1,47 @@
 package simulation;
 
-public class Animal {
+import java.util.Random;
+
+public class Animal implements Comparable<Animal>{
     private Vector2D position;
     private int energy;
-    private int age;
+    private int age =1;
+    private final int animalID;
+    private static int counter = 0;
+    private final Genome genome;
+    private int numberOfChildren = 0;
 
     public Animal(Vector2D position, int energy) {
 
         this.position = position;
         this.energy = energy;
-        this.age = 1;
+        this.animalID = counter ++;
+        this.genome = new Genome();
+
+    }
+
+    public Animal(Animal mother, Animal father){
+        Vector2D move = MapDirection.values()[new Random().nextInt(MapDirection.values().length)].getUnitVector();
+        this.position = pbc(mother.getPosition().add(move));
+        this.energy = (mother.getEnergy() + father.getEnergy())/4;
+        this.animalID = counter ++;
+        this.genome = new Genome(mother.getGenome(), father.getGenome());
+        mother.setEnergy(mother.getEnergy() * 3/4);
+        father.setEnergy(father.getEnergy() * 3/4);
+        mother.increaseNumberOfChildren();
+        father.increaseNumberOfChildren();
+    }
+
+    public Genome getGenome() {
+        return genome;
+    }
+
+    public int getNumberOfChildren() {
+        return numberOfChildren;
+    }
+
+    public void increaseNumberOfChildren(){
+        numberOfChildren ++;
     }
 
     public Vector2D getPosition() {
@@ -18,6 +50,10 @@ public class Animal {
 
     public int getEnergy(){
         return energy;
+    }
+
+    public int getAnimalID() {
+        return animalID;
     }
 
     public Animal setEnergy(int newEnergy) {
@@ -35,7 +71,7 @@ public class Animal {
 
     public void move(MapDirection direction) {
         position = pbc(position.add(direction.getUnitVector()));
-        System.out.println("Animal moves " + direction + ": new position: " + position +
+        System.out.println("Animal moves " + animalID + " "+ direction + ": new position: " + position +
                 ": energy level: " + energy + " :age: " + age);
     }
 
@@ -48,5 +84,10 @@ public class Animal {
         if (position.getY() >= height) return position.subtract(new Vector2D(0, height));
 
         return position;
+    }
+
+    public int compareTo(Animal animal){
+        return getEnergy() == animal.getEnergy() ? getAnimalID() - animal.getAnimalID() : getEnergy() - animal.getEnergy();
+
     }
 }
